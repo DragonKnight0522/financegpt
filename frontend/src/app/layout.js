681e6ecab7c0fbb62da3d8d1ebb97a76cf079616
Dragon Knight <139461215+DragonKnight0522@users.noreply.github.com>
2023-09-29 14:15:16 +0900

    Update layout.js

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -7,7 +7,7 @@ import store from "../store";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
-export const metadata = {
+const metadata = {
 	description:
 		"This personal finance dashboard; chat uses Plaid API requests; Firebase Auth, Collections to aggregate account info across multiple accounts. Designed for personal use and storage. Insights provided by GPT4",
 	title: "AI enabled personal accountant",
